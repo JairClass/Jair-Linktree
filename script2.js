0d00eb1a6f9dc5d3053a168aa54cd0d9c538ee09
Jair Willian Class <j.willianclass02@gmail.com>
2024-01-08 14:25:19 -0300

    added files and updated the code

---
+++ script2.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    document.querySelector(".loader").style.opacity = 0;
+    setTimeout(() => {
+        document.querySelector(".loader").style.display = "none"; 
+    }, 1000);
+     // Seleciona o vídeo e o botão
+    const video = document.querySelector("video");
+    const videoControlButton = document.getElementById("videoControl");
+
+     // Função para alternar o estado de reprodução do vídeo
+    function toggleVideoPlayback() {
+        if (video.paused) {
+            video.play();
+            videoControlButton.textContent = 'Pausar';
+        } else {
+            video.pause();
+            videoControlButton.textContent = 'Reproduzir';
+        }
+    }
+
+     // Adiciona o evento de clique no botão
+    videoControlButton.addEventListener('click', toggleVideoPlayback);
+};
+
